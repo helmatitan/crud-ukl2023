e73e90f8b9a8caeb56a90a6d3f238bc191399939
helmatitan <fameliahelma@gmail.com>
2023-05-24 17:08:12 +0700

    ya

--- koneksitoko.php
+++ koneksitoko.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','','toko_online');
+$conn=mysqli_connect('localhost','root','','shop');
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
